81ff3914a94d0d1816b6d52bde632196f900694b
Chmouel Boudjnah <chmouel@chmouel.com>
2022-11-21 21:05:00 +0100

    buggy
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![warn(clippy::all, clippy::pedantic)]
 #![allow(clippy::too_many_lines)]
-#![allow(clippy::cast_possible_truncation)]
-#![allow(clippy::map_unwrap_or)]
 #![forbid(unsafe_code)]
 
 use std::sync::Arc;
