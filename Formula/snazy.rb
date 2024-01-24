e79951b5b3999c4081bb7934680acf1fc7d2b0a3
Chmouel <chmouel@chmouel.com>
2024-01-24 14:08:13 +0000

    Formula Bump snazy to 0.52.15

--- Formula/snazy.rb
+++ Formula/snazy.rb
@@ -5,11 +5,11 @@
 class Snazy < Formula
   desc "snazy - a snazzy json log viewer"
   homepage "https://github.com/chmouel/snazy"
-  version "0.52.14"
+  version "0.52.15"
 
   on_macos do
-    url "https://github.com/chmouel/snazy/releases/download/0.52.14/snazy-v0.52.14-macos.tar.gz"
-    sha256 "ba3fce1ee3698d78d53b55d994fb8a3ea2a993bc70f3b5a4e0257c9514daf408"
+    url "https://github.com/chmouel/snazy/releases/download/0.52.15/snazy-v0.52.15-macos.tar.gz"
+    sha256 "0d62fef643c82448869d451857c6b5731645f4296c48634e2c36932f606c0f63"
 
     def install
       bin.install "snazy" => "snazy"
@@ -28,8 +28,8 @@ class Snazy < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/chmouel/snazy/releases/download/0.52.14/snazy-v0.52.14-linux-amd64.tar.gz"
-      sha256 "c80450607b5caaa7797d73f96502be64428e1c87bb93c722d8ba19a0048f0c25"
+      url "https://github.com/chmouel/snazy/releases/download/0.52.15/snazy-v0.52.15-linux-amd64.tar.gz"
+      sha256 "bd589fe14687f9ac4092e23c0a9031a5f3249aee8e6eab1ea0e56e04230bbd10"
 
       def install
         bin.install "snazy" => "snazy"
@@ -46,8 +46,8 @@ class Snazy < Formula
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/chmouel/snazy/releases/download/0.52.14/snazy-v0.52.14-linux-arm64.tar.gz"
-      sha256 "26c8accc5a77e342aac688d7ee0a507c509226fa4c86f13aed693bdd62dae510"
+      url "https://github.com/chmouel/snazy/releases/download/0.52.15/snazy-v0.52.15-linux-arm64.tar.gz"
+      sha256 "610ada38b0e6ab3794cab1a0feeb8551d7bb425660b93b7cae43d11a8418473d"
 
       def install
         bin.install "snazy" => "snazy"
